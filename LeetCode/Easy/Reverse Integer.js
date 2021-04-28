5e9bbf074746b4e785d1a21a608b03599c5c8ec6
Jacky Tang <jackytang74@gmail.com>
2021-04-28 16:16:54 -0700

    LeetCode Easy Reverse Integer mathematical method and breakdown

--- LeetCode/Easy/Reverse Integer.js
+++ LeetCode/Easy/Reverse Integer.js
@@ -39,7 +39,6 @@ var reverse = function(x) {
 
 //String Approach small improvements
 var reverse = function(x) {
-    // problem states that our reversed number has to stay within bounds, so we will use these variables to compare 
     const MAX = Math.pow(2, 31) - 1;
     const MIN = -1 * Math.pow(2, 31);
     
@@ -51,3 +50,31 @@ var reverse = function(x) {
     // by using a ternary operator, we save some lines of code
     return (reversedNum > MAX || reversedNum < MIN) ? 0 : reversedNum;
 };
+
+//Numerical Solution
+var reverse = function(x) {
+    const MAX = Math.pow(2, 31) - 1;
+    const MIN = -1 * Math.pow(2, 31);
+    // tracking our answer
+    let result = 0;
+    // we want our process to repeat until the original number x is gone and completely reversed so we use a while loop
+    while(x !== 0) {
+        // we don't need this variable, it just makes the code look a little cleaner
+        let popped = x % 10;
+        //we have to use Math.trunc instead of Math.floor due to the possibility of x being negative. 
+            // If we use Math.floor then the while loop would be never ending since x would evnetually resolve -1 and stay that way
+        x = Math.trunc(x / 10);
+        //by multiplying by 10 and adding the popped number, we begin to reverse x
+        result = result * 10 + popped;
+    }
+    // this check can happen inside the while loop, and probably should be inside the while loop if x grows into a large number
+        // do you want to check AFTER you reversed the entire number or check every while iteration and stop the loop if the number grows out of bounds, depends on the number right?
+    if (result > MAX || result < MIN) {
+        return 0;
+    }
+
+    return result;
+};
+
+//The numerical approach will give slower results on LeetCode, but you save space by not having to make an array. 
+    // String method for smaller numbers and math method for larger numbers
