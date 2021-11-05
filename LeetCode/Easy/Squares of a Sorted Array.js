e90b3e99d9e8b5dc1440402295d36c07bbe5df13
Jacky Tang <jackytang74@gmail.com>
2021-11-04 21:08:37 -0700

    LeetCode Easy Squares of a Sorted Array solution and breakdown

---
+++ LeetCode/Easy/Squares of a Sorted Array.js
@@ -0,0 +1,8 @@
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+var sortedSquares = function(nums) {
+    // this would be the most obvious method, iterate through the array and map the elements with a square function
+    const squaredNums = nums.map(num => num * num);
+    // then sort the array with it's new elements and return
+    return squaredNums.sort((a, b) => a - b);
+};
