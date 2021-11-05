408497834b167a43444ebc4e7983c1a7de419482
Jacky Tang <jackytang74@gmail.com>
2021-11-04 22:28:11 -0700

    LeetCode Medium Rotate Array more time conscious solution and breakdown

--- LeetCode/Medium/Rotate Array.js
+++ LeetCode/Medium/Rotate Array.js
@@ -9,3 +9,20 @@ var rotate = function(nums, k) {
     return nums.unshift(...rotatedPart);
 };
 
+var rotate = function(nums, k) {
+    // same as before, we are accoutn of cases where k is larger than nums.length
+    k = k % nums.length;
+    // we extend our nums array, duplicating the beginning
+        // [1, 2, 3, 4, 5] with k = 3 becomes [1, 2, 3, 1, 2, 3, 4, 5]
+    for(let i = nums.length - 1; i >= 0; i--) {
+        nums[i + k] = nums[i];
+    }
+    // then we remove each element at the end and place them in the apprpriate place, eliminating the duplicated beginning
+        // [1, 2, 3, 1, 2, 3, 4, 5] => [1, 2, 5, 1, 2, 3, 4] => [1, 4, 5, 1, 2, 3] => [3, 4, 5, 1, 2]
+    for(let j = k - 1; j >= 0; j--) {
+        nums[j] = nums.pop();
+    }
+
+    return nums;
+};
+// O(n + k): we iterate the length of the original array, then we have a loop that iterates through the length of k
