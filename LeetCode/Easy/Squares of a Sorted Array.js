a3225cdcbeebde36862d7b0fa182a2848fa59056
Jacky Tang <jackytang74@gmail.com>
2021-11-04 21:09:30 -0700

    LeetCode Easy Squares of a Sorted Array O(n) time complexity and breakdown

--- LeetCode/Easy/Squares of a Sorted Array.js
+++ LeetCode/Easy/Squares of a Sorted Array.js
@@ -9,3 +9,30 @@ var sortedSquares = function(nums) {
 
 // ES6 one liner
 const sortedSquares = (nums) => nums.map(num => num * num).sort((a, b) => a - b);
+
+// a more elegant method, only O(n) time complexity
+var sortedSquares = function(nums) {
+    // we create a new array to store the new elements
+    const squaredNums = [];
+    // we have two variables, i and j tracking the lowest and highest index of our nums array respectively
+    let i = 0, j = nums.length - 1;
+    
+    while (i <= j) {
+        // check whether the left number is greater than or equal to the right number
+            // this is to account for negatives becoming positive after quaring them
+        if (Math.abs(nums[i]) >= Math.abs(nums[j])) {
+            // if they are then square the number and place it at the appropriate index.
+                // by using j - i and the if check, we are effectively finding the highest number each time and placing it as far down the array as possible
+            squaredNums[j - i] = nums[i] * nums[i];
+            // increment the left tracker
+            i += 1;
+        } else {
+            // otherwise, the right number is larger, square it and place it accordingly,
+            squaredNums[j - i] = nums[j] * nums[j];
+            // decrememnt the right tracker
+            j -= 1;
+        }
+    }
+    // return the final array, squared and sorted
+    return squaredNums;
+}
