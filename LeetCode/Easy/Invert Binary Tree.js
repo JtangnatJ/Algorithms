67a0447a8eec27effdf6a23e043868df352d63e7
Jacky Tang <jackytang74@gmail.com>
2022-05-24 15:29:38 -0700

    LeetCode Easy Invert Binary Tree recursion breakdown

--- LeetCode/Easy/Invert Binary Tree.js
+++ LeetCode/Easy/Invert Binary Tree.js
@@ -2,11 +2,15 @@
 
 //recursion
 var invertTree = function(root) {
+    // if the root does not exist then no point running the function
     if (root === null) {
         return root;
     }
 
+    // we switch the left and the right, and call the function itself on the children
+        // this will do down each branch and switch all consquent children as well
     [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
 
     return root;
 };
+
