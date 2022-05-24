b10e9fc5dcd79da0f812f12704d4dd101df9932c
Jacky Tang <jackytang74@gmail.com>
2022-05-24 13:51:58 -0700

    LeetCode Easy Valid Palindrome solution and breakdown

---
+++ LeetCode/Easy/Valid Palindrome.js
@@ -0,0 +1,15 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+//     it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+    // Given a string s, return true if it is a palindrome, or false otherwise.
+
+// reverse approach
+var isPalindrome = function(s) {
+    // make all letters lowercase and use regex to replace the white spaces and non alphanumeric characters
+    const strippedString = s.toLowerCase().replace(/\W|_/g, '');
+
+    // generate the reverse of the stripped string
+    const reversedString = strippedString.split('').reverse().join('');
+
+    // return the comparison
+    return strippedString === reversedString;
+};
