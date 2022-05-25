4fb69f659104ea8f59f9cc836fd438a88a555a6b
Jacky Tang <jackytang74@gmail.com>
2022-05-25 13:56:42 -0700

    LeetCode Easy Lowest Common Ancestor of a Binary Tree recursive method

--- LeetCode/Easy/Lowest Common Ancestor of a Binary Search Tree.js
+++ LeetCode/Easy/Lowest Common Ancestor of a Binary Search Tree.js
@@ -26,3 +26,13 @@ var lowestCommonAncestor = function(root, p, q) {
     return root;
 };
 
+// recursive method
+var lowestCommonAncestor = function(root, p, q) {
+    if (root.val < p.val && root.val < q.val) {
+        return lowestCommonAncestor(root.right, p, q);
+    }
+    if (root.val > p.val && root.val > q.val) {
+        return lowestCommonAncestor(root.left, p, q);
+    }
+    return root;
+};
