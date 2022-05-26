54679a8c395a649021801c4ea8619221425b516c
Jacky Tang <jackytang74@gmail.com>
2022-05-26 13:21:29 -0700

    LeetCode Easy Linked List Cycle breakdown

--- LeetCode/Easy/Linked List Cycle.js
+++ LeetCode/Easy/Linked List Cycle.js
@@ -5,18 +5,25 @@
 
 // Return true if there is a cycle in the linked list. Otherwise, return false.
 
+// we are essentially checking if the linked list loops
 var hasCycle = function(head) {
+    // use two pointers to track two speeds of traversing the linked list
+        // techinically don't need to make both, can use head for one of the variables, just nice to be desctriptive
     let slow = head;
     let fast = head;
     
+    // since the fast variable "races down the tracks" we only want to run this while loop when fast has value
+        // if fast ever becomes null, then there is no loop in the linked list
     while(fast && fast.next) {
+        // traverse down the linked list, slow goes one at a time, fast goes two at a time
         slow = slow.next;
         fast = fast.next.next;
-        
+        // if they are ever the same value then it is because fast looped around and caught slow on a return loop
         if (slow === fast) {
             return true;
         }
     }
     
+    // if the while loop ever ends, it will be because fast saw a null node: meaning the linked list has no loop
     return false;
 };
